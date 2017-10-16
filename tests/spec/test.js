4077cee1c4846ddd496e202c78f3fdb74ee65412
Xotic750 <xotic750@gmail.com>
2017-10-16 17:07:49 +0200

    Update for ES5 to ES2018 white space specifications

--- tests/spec/test.js
+++ tests/spec/test.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var trim;
+var lib;
 if (typeof module === 'object' && module.exports) {
   require('es5-shim');
   require('es5-shim/es5-sham');
@@ -16,72 +16,155 @@ if (typeof module === 'object' && module.exports) {
       obj.shim();
     }
   });
-  trim = require('../../index.js');
+  lib = require('../../index.js');
 } else {
-  trim = returnExports;
+  lib = returnExports;
 }
+
+var trim = lib.trim;
+var trim2016 = lib.trim2016;
+var trim2018 = lib.trim2018;
 
 var hasSymbol = typeof Symbol === 'function' && typeof Symbol('') === 'symbol';
 var ifSymbolIt = hasSymbol ? it : xit;
 
 describe('trim', function () {
-  it('is a function', function () {
-    expect(typeof trim).toBe('function');
+  var allWhitespaceChars2016 = '\x09\x0A\x0B\x0C\x0D\x20\xA0\u1680\u180E\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u202F\u205F\u3000\u2028\u2029\uFEFF';
+  var allWhitespaceChars2018 = '\x09\x0A\x0B\x0C\x0D\x20\xA0\u1680\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u202F\u205F\u3000\u2028\u2029\uFEFF';
+
+  describe('basic', function () {
+    it('trim is a function', function () {
+      expect(typeof trim).toBe('function');
+    });
+
+    it('trim is a function', function () {
+      expect(typeof trim2016).toBe('function');
+    });
+
+    it('trim is a function', function () {
+      expect(typeof trim2018).toBe('function');
+    });
+
+    it('trim should be trim2018', function () {
+      expect(trim).toBe(trim2018);
+    });
+
+    it('trim should not be trim2016', function () {
+      expect(trim).not.toBe(trim2016);
+    });
   });
 
-  it('should throw when target is null or undefined', function () {
-    expect(function () {
-      trim();
-    }).toThrow();
+  describe('trim2016', function () {
+    it('should throw when target is null or undefined', function () {
+      expect(function () {
+        trim2016();
+      }).toThrow();
 
-    expect(function () {
-      trim(void 0);
-    }).toThrow();
+      expect(function () {
+        trim2016(void 0);
+      }).toThrow();
 
-    expect(function () {
-      trim(null);
-    }).toThrow();
+      expect(function () {
+        trim2016(null);
+      }).toThrow();
+    });
+
+    it('Basic tests', function () {
+      expect(trim2016(' \t\na \t\n')).toBe('a', 'strips whitespace off the left and right side');
+      expect(trim2016('a')).toBe('a', 'noop when no whitespace');
+      expect(trim2016(allWhitespaceChars2016 + 'a' + allWhitespaceChars2016)).toBe('a', 'all expected whitespace chars are trimmed');
+      var zeroWidth = '\u200b';
+      expect(trim(zeroWidth)).toBe(zeroWidth, 'zero width space does not trim');
+    });
+
+    it('should return a string for everything', function () {
+      var values = [
+        true,
+        'abc',
+        1,
+        function () {},
+        [],
+        /r/
+      ];
+
+      var expected = values.map(String);
+      var actual = values.map(trim2016);
+      expect(actual).toEqual(expected);
+    });
+
+    it('should throw for Object.create(null)', function () {
+      expect(function () {
+        trim2016(Object.create(null));
+      }).toThrow();
+    });
+
+    ifSymbolIt('should throw for Symbol', function () {
+      var sym = Symbol('foo');
+      expect(function () {
+        trim2016(sym);
+      }).toThrow();
+
+      var symObj = Object(sym);
+      expect(function () {
+        trim2016(Object(symObj));
+      }).toThrow();
+    });
   });
 
-  it('Basic tests', function () {
-    expect(trim(' \t\na \t\n')).toBe('a', 'strips whitespace off both sides');
-    expect(trim('a')).toBe('a', 'noop when no whitespace');
-    var allWhitespaceChars = '\x09\x0A\x0B\x0C\x0D\x20\xA0\u1680\u180E\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u202F\u205F\u3000\u2028\u2029\uFEFF';
-    expect(trim(allWhitespaceChars + 'a' + allWhitespaceChars)).toBe('a', 'all expected whitespace chars are trimmed');
-    var zeroWidth = '\u200b';
-    expect(trim(zeroWidth)).toBe(zeroWidth, 'zero width space does not trim');
-  });
+  describe('trim2018', function () {
+    it('should throw when target is null or undefined', function () {
+      expect(function () {
+        trim2018();
+      }).toThrow();
 
-  it('should return a string for everything', function () {
-    var values = [
-      true,
-      'abc',
-      1,
-      function () {},
-      [],
-      /r/
-    ];
+      expect(function () {
+        trim2018(void 0);
+      }).toThrow();
 
-    var expected = values.map(String);
-    var actual = values.map(trim);
-    expect(actual).toEqual(expected);
-  });
+      expect(function () {
+        trim2018(null);
+      }).toThrow();
+    });
 
-  it('should throw for Object.create(null)', function () {
-    expect(function () {
-      trim(Object.create(null));
-    }).toThrow();
-  });
+    it('Basic tests', function () {
+      expect(trim2018(' \t\na \t\n')).toBe('a', 'strips whitespace off the left and right side');
+      expect(trim2018('a')).toBe('a', 'noop when no whitespace');
+      expect(trim2018(allWhitespaceChars2018 + 'a' + allWhitespaceChars2018)).toBe('a', 'all expected whitespace chars are trimmed');
+      var zeroWidth = '\u200b';
+      expect(trim(zeroWidth)).toBe(zeroWidth, 'zero width space does not trim');
+    });
 
-  ifSymbolIt('should throw for Symbol', function () {
-    var sym = Symbol('foo');
-    expect(function () {
-      trim(sym);
-    }).toThrow();
+    it('should return a string for everything', function () {
+      var values = [
+        true,
+        'abc',
+        1,
+        function () {},
+        [],
+        /r/
+      ];
 
-    var symObj = Object(sym);
-    expect(function () {
-      trim(Object(symObj));
-    }).toThrow();
+      var expected = values.map(String);
+      var actual = values.map(trim2018);
+      expect(actual).toEqual(expected);
+    });
+
+    it('should throw for Object.create(null)', function () {
+      expect(function () {
+        trim2018(Object.create(null));
+      }).toThrow();
+    });
+
+    ifSymbolIt('should throw for Symbol', function () {
+      var sym = Symbol('foo');
+      expect(function () {
+        trim2018(sym);
+      }).toThrow();
+
+      var symObj = Object(sym);
+      expect(function () {
+        trim2018(Object(symObj));
+      }).toThrow();
+    });
   });
 });
