f50abd76b1ef99679df376955b919bfc96ee7e92
Graham Fairweather <xotic750@gmail.com>
2019-07-27 23:08:22 +0200

    Cleanup

--- dist/trim-x.js
+++ dist/trim-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-07-27T13:30:38.084Z",
+  "date": "2019-07-27T21:08:16.920Z",
   "describe": "",
   "description": "This method removes whitespace from the left and right end of a string.",
   "file": "trim-x.js",
