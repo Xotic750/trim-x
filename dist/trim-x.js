5bf02b04d27f74a4bb320bb4d383a4d2009920aa
Graham Fairweather <xotic750@gmail.com>
2019-07-29 23:44:23 +0200

    Rule changes

--- dist/trim-x.js
+++ dist/trim-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-07-29T18:45:18.428Z",
+  "date": "2019-07-29T21:44:18.561Z",
   "describe": "",
   "description": "This method removes whitespace from the left and right end of a string.",
   "file": "trim-x.js",
