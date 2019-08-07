254a0e36b42cdbd211e2d4cd670c0f900bdaf410
Graham Fairweather <xotic750@gmail.com>
2019-08-07 23:49:22 +0200

    Lodash security fix

--- dist/trim-x.js
+++ dist/trim-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-08-07T00:59:35.601Z",
+  "date": "2019-08-07T21:49:16.633Z",
   "describe": "",
   "description": "This method removes whitespace from the left and right end of a string.",
   "file": "trim-x.js",
